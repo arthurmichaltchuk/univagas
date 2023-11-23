b0ae07bb8bc60e4428e745437a24bc7dc618548b
Mateus Zancheta <mateuszancheta2005@gmail.com>
2023-11-23 15:30:01 -0300

    Organizando tela mentoria

--- src/componentes/mentorias.jsx
+++ src/componentes/mentorias.jsx
@@ -15,15 +15,16 @@ export default function Mentorias() {
             <h2>R$ 25,00</h2>
             <label>todo mês</label>
             <p>Perfeito para iniciantes</p>
-          </div>
-          <button className="compre-agora">Compre agora</button>
 
-          <label className="descricao" id="descOne">
-            • Filtragem de oportunidades de estágio e emprego
-          </label>
-          <label className="descricao">
-            • Dicas sobre carreira e processo seletivo.
-          </label>
+            <button className="compre-agora">Compre agora</button>
+
+            <label className="descricao" id="descOne">
+              Filtragem de oportunidades de estágio e emprego
+            </label>
+            <label className="descricao">
+              Dicas sobre carreira e processo seletivo.
+            </label>
+          </div>
         </div>
 
         <div className="container-plano">
@@ -32,24 +33,25 @@ export default function Mentorias() {
             <h2>R$ 82,90</h2>
             <label>todo mês</label>
             <p>Para entusiastas sérios</p>
-          </div>
-          <button className="compre-agora" id="compre-agora2">
-            Compre agora
-          </button>
 
-          <label className="descricao" id="descOne">
-            • Mentoria com um profissional experiente.
-          </label>
-          <label className="descricao">
-            • Feedbacks detalhados de empresas pós-entrevistas.
-          </label>
-          <label className="descricao">
-            • Participação em eventos presenciais de networking.
-          </label>
-          <label className="descricao">
-            • Possibilidade de realizar simulados de entrevistas com
-            profissionais.
-          </label>
+            <button className="compre-agora" id="compre-agora2">
+              Compre agora
+            </button>
+
+            <label className="descricao" id="descOne">
+              Mentoria com um profissional experiente.
+            </label>
+            <label className="descricao">
+              Feedbacks detalhados de empresas pós-entrevistas.
+            </label>
+            <label className="descricao">
+              Participação em eventos presenciais de networking.
+            </label>
+            <label className="descricao">
+              Possibilidade de realizar simulados de entrevistas com
+              profissionais.
+            </label>
+          </div>
         </div>
 
         <div className="container-plano">
@@ -58,28 +60,30 @@ export default function Mentorias() {
             <h2>R$ 123,40</h2>
             <label>todo mês</label>
             <p>Quando apenas o melhor servirá</p>
-          </div>
-          <button className="compre-agora">Compre agora</button>
 
-          <label className="descricao" id="descOne">
-            • Relatório personalizado de desempenho em entrevistas.
-          </label>
-          <label className="descricao">
-            • Workshops exclusivos de desenvolvimento profissional.
-          </label>
-          <label className="descricao">
-            • Acesso a conteúdo premium de parceiros educacionais.
-          </label>
-          <label className="descricao">
-            • Participação em grupos de discussão exclusivos.
-          </label>
-          <label className="descricao">
-            • Suporte dedicado para dúvidas e questões urgentes.
-          </label>
-          <label className="descricao">
-            • Acesso a eventos exclusivos de empresas patrocinadoras.
-          </label>
+            <button className="compre-agora">Compre agora</button>
+
+            <label className="descricao" id="descOne">
+              Relatório personalizado de desempenho em entrevistas.
+            </label>
+            <label className="descricao">
+              Workshops exclusivos de desenvolvimento profissional.
+            </label>
+            <label className="descricao">
+              Acesso a conteúdo premium de parceiros educacionais.
+            </label>
+            <label className="descricao">
+              Participação em grupos de discussão exclusivos.
+            </label>
+            <label className="descricao">
+              Suporte dedicado para dúvidas e questões urgentes.
+            </label>
+            <label className="descricao">
+              Acesso a eventos exclusivos de empresas patrocinadoras.
+            </label>
+          </div>
         </div>
+        
       </section>
     </div>
   );
