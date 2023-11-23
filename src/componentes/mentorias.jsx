5600179e6dec5d368c27ed5c1e9942aebf1fb72d
Mateus Zancheta <mateuszancheta2005@gmail.com>
2023-11-23 16:15:32 -0300

    Tela mentorias feito

--- src/componentes/mentorias.jsx
+++ src/componentes/mentorias.jsx
@@ -5,7 +5,7 @@ import { FaCheck } from "react-icons/fa";
 export default function Mentorias() {
   return (
     <div className="container">
-      <h1 id="text-mentoria">Escolha seu plano de preços<IoMdClose /><FaCheck /></h1>
+      <h1 id="text-mentoria">Escolha seu plano de preços</h1>
       <div className="header-mentoria">
         <div id="subtitle-mentoria">
           <p>Melhor preço</p>
@@ -20,14 +20,30 @@ export default function Mentorias() {
             <label>todo mês</label>
             <p>Perfeito para iniciantes</p>
 
-            <button className="compre-agora">Compre agora</button>
-
             <label className="descricao" id="descOne">
+              <FaCheck />
               Filtragem de oportunidades de estágio e emprego
             </label>
             <label className="descricao">
-              Dicas sobre carreira e processo seletivo.
+              <FaCheck />2 Dicas sobre carreira e processo seletivo.
             </label>
+            <label className="descricao">
+              <FaCheck />
+              Possibilidade de realizar simulados de entrevistas com
+              profissionais.
+            </label>
+            <label className="descricao">
+              <IoMdClose />
+              Acesso a eventos exclusivos de empresas patrocinadoras.
+            </label>
+            <label className="descricao">
+              <IoMdClose />
+              Acesso prioritário a vagas.
+            </label>
+
+            <button className="compre-agora" id="compre-agora1">
+              Compre agora
+            </button>
           </div>
         </div>
 
@@ -38,23 +54,29 @@ export default function Mentorias() {
             <label>todo mês</label>
             <p>Para entusiastas sérios</p>
 
-            <button className="compre-agora" id="compre-agora2">
-              Compre agora
-            </button>
-
             <label className="descricao" id="descOne">
-              Mentoria com um profissional experiente.
+              <FaCheck />
+              Filtragem de oportunidades de estágio e emprego.
             </label>
             <label className="descricao">
-              Feedbacks detalhados de empresas pós-entrevistas.
+              <FaCheck />5 Dicas sobre carreira e processo seletivo.
             </label>
             <label className="descricao">
-              Participação em eventos presenciais de networking.
-            </label>
-            <label className="descricao">
+              <FaCheck />
               Possibilidade de realizar simulados de entrevistas com
               profissionais.
             </label>
+            <label className="descricao">
+              <FaCheck />
+              Acesso a eventos exclusivos de empresas patrocinadoras.
+            </label>
+            <label className="descricao">
+              <IoMdClose />
+              Acesso prioritário a vagas.
+            </label>
+            <button className="compre-agora" id="compre-agora2">
+              Compre agora
+            </button>
           </div>
         </div>
 
@@ -65,29 +87,30 @@ export default function Mentorias() {
             <label>todo mês</label>
             <p>Quando apenas o melhor servirá</p>
 
-            <button className="compre-agora">Compre agora</button>
-
             <label className="descricao" id="descOne">
+              <FaCheck />
               Relatório personalizado de desempenho em entrevistas.
             </label>
             <label className="descricao">
-              Workshops exclusivos de desenvolvimento profissional.
+              <FaCheck />
+              10 Dicas sobre carreira e processo seletivo.
             </label>
             <label className="descricao">
-              Acesso a conteúdo premium de parceiros educacionais.
-            </label>
-            <label className="descricao">
+              <FaCheck />
               Participação em grupos de discussão exclusivos.
             </label>
             <label className="descricao">
-              Suporte dedicado para dúvidas e questões urgentes.
+              <FaCheck />
+              Possibilidade de realizar simulados de entrevistas com
+              profissionais.
             </label>
             <label className="descricao">
-              Acesso a eventos exclusivos de empresas patrocinadoras.
+              <FaCheck />
+              Acesso prioritário a vagas.
             </label>
+            <button className="compre-agora">Compre agora</button>
           </div>
         </div>
-        
       </section>
     </div>
   );
